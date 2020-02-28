c5a7e411e481e1221c2ec8bf231db4c18874ebbf
Matthew Barbano <meb100@duke.edu>
2020-02-27 21:38:02 -0500

    Lambda side of functional image sending

--- ComponentCheckout_Lambda.py
+++ ComponentCheckout_Lambda.py
@@ -1,9 +1,8 @@
 import greengrasssdk  # SDK that exists in the core, not in Cloud
 import json
+import Connect_Lambda
 
 def handler(event, context):
-    client = greengrasssdk.client('iot-data')
-    client.publish(
-        topic='component/storage/lambda/to/device',
-        qos=0,
-        payload=json.dumps(event))
+    # print("Receiving: " + json.dumps(event))
+    # event is a dictionary with the "Number" and "Data" entries
+    Connect_Lambda.receiveImageBlock(event)
