1a015ee26e4c5443bfd10bf60a5df2e9dc7d2cd3
princeton-edge-lab <edgelabgithub@gmail.com>
2020-03-20 19:15:00 +0000

    Core functionality, pi side

--- ComponentCheckout_Pi.py
+++ ComponentCheckout_Pi.py
@@ -4,12 +4,16 @@ Code taken and modified from:
 https://www.raspberrypi.org/documentation/usage/gpio/python/README.md
 https://picamera.readthedocs.io/en/release-1.13/recipes1.html
 '''
+from PIL import Image
 import RPi.GPIO as GPIO
 from gpiozero import Button, LED
 from time import sleep
 from picamera import PiCamera
 import Connect_Pi
 import json
+import pickle
+import string
+from sklearn import neighbors
 
 LED_1_PIN = 2
 LED_2_PIN = 3
@@ -22,7 +26,7 @@ button = Button(BUTTON_PIN)
 camera = PiCamera()
 
 def main():
-	Connect_Pi.connect(blinkLED)
+	Connect_Pi.connect(classifyAndBlinkLED)
 	setupCamera()
 	# setupGPIO()
 
@@ -47,20 +51,34 @@ def setupGPIO():
 	GPIO.setup(LED_3_PIN, GPIO.OUT)
 	GPIO.setup(BUTTON_PIN, GPIO.IN)
 '''
-def blinkLED(messageDictionary):
-	pass
-	# GPIO.output([identifier_to_led[messageDictionary["ComponentName"]]], GPIO.HIGH)
+def classifyAndBlinkLED(messageDictionary):
+	MODEL_FILE = "trained_model"
+	
+	print("Loaded trained model")
+	trained_model_file = open(MODEL_FILE, 'rb')
+	classifier = pickle.load(trained_model_file)
+	print("Model loaded")
+	
+	print("Classifying")
+	# no_brackets = string.replace(string.replace(messageDictionary["Data"], "[", ""), "]", "")
+	# feature_vector = no_brackets.split(", ")
+	feature_vector = messageDictionary["Data"]
+	result = classifier.predict([feature_vector])
+	print("Classification complete. Result: " + result[0])
+	
+	# GPIO.output([identifier_to_led[result[0]]], GPIO.HIGH)
 	# sleep(2)
-	# GPIO.output([identifier_to_led[messageDictionary["ComponentName"]]], GPIO.LOW)
-	# print('Blinked LED corresponding to ' + messageDictionary["ComponentName"])
+	# GPIO.output([identifier_to_led[result[0]]], GPIO.LOW)
+	# print('Blinked LED corresponding to ' + result[0])
 
 def setupCamera():
-	camera.resolution = (1024, 768)
+	camera.resolution = (400, 700) # (1024, 768)
 	sleep(2)
 
 def takePicture():
 	camera.capture(PICTURE_FILENAME)
-	return open(PICTURE_FILENAME, "rb")
+	# return open(PICTURE_FILENAME, "rb")
+	return Image.open(PICTURE_FILENAME)
 
 def closePictureFile(pictureFile):
 	pictureFile.close()
