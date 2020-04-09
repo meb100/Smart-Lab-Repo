da18c783ba421c638e1c6c10b272f9b3d7f8ef12
Matthew Barbano <meb100@duke.edu>
2020-04-09 02:57:36 -0400

    Final version from Lambda

--- Connect_Lambda.py
+++ Connect_Lambda.py
@@ -3,46 +3,22 @@ import json
 import FeatureExtractor_Lambda
 import time
 
-numBlocks = -1
-blocksProcessed = 0
-imageBlocks = []
-feature_extraction_start_time = 0.0
+
 client = greengrasssdk.client('iot-data')
 
-def receiveImageBlock(messageDictionary):
-    global numBlocks
-    global imageBlocks
-    global blocksProcessed
-    global feature_extraction_start_time
+def receiveImageBlock(parsed, decompress_elapsed_time):
+    feature_extraction_start_time = 0.0
+    feature_extraction_start_time = time.time()
+    FeatureExtractor_Lambda.extract_from_block(parsed)
+    feature_extraction_elapsed_time = time.time() - feature_extraction_start_time
 
-    if messageDictionary["Description"] == "Num Blocks":   # TODO could do all this at end of previous cycle
-        print("In Num Blocks block")
-        setup_start_time = time.time()
-        blocksProcessed = 0
-        numBlocks = int(messageDictionary["Data"])
-        FeatureExtractor_Lambda.feature_vector = [0, 0, 0]
-        FeatureExtractor_Lambda.non_background_pixels = 0
-        setup_end_time = time.time()
-        setup_elapsed_time = setup_end_time - setup_start_time
-        publish({"Description": "Num Blocks Ack", "Time": str(setup_elapsed_time)})
-    else:
-        blocksProcessed += 1
-        if blocksProcessed == 1:
-            feature_extraction_start_time = time.time()
-        FeatureExtractor_Lambda.extract_from_block(messageDictionary["Data"])
-        print("Block number ", int(messageDictionary["Description"]))
-        print("Feature vector after update:")
-        print(FeatureExtractor_Lambda.feature_vector)
-        if blocksProcessed == numBlocks:
-            FeatureExtractor_Lambda.normalize_feature_vector()
-            feature_extraction_elapsed_time = time.time() - feature_extraction_start_time
-            msgDict = {"Description": "Feature Vector", "Data": FeatureExtractor_Lambda.feature_vector, "Time": str(feature_extraction_elapsed_time)}
-            publish(msgDict)
+    msgDict = {"Feature Vector": FeatureExtractor_Lambda.feature_vector_dictionary,
+    "DC Time": decompress_elapsed_time, "FV Time": feature_extraction_elapsed_time}
+    publish(msgDict)
 
 def publish(messageDictionary):
     jsonString = json.dumps(messageDictionary)
-    print("Publishing: " + jsonString)
     client.publish(
-        topic='component/storage/lambda/to/device',
+        topic='component/storage/lambda/to/device/0',
         qos=0,
         payload=jsonString)
--- FeatureExtractor_Lambda.py
+++ FeatureExtractor_Lambda.py
@@ -1,56 +1,24 @@
-
-feature_vector = [0, 0, 0]
-non_background_pixels = 0
-
-# Input: "1,2,3/1,2,3/1,2,3-1,2,3/1,2,3/1,2,3"
-# Input: [{"Data": "1,2,3/1,2,3/1,2,3-1,2,3/1,2,3/1,2,3", "Description": 23}, ..., ...] - each block is a row
-'''
-def extract_from_block(block):
-    global feature_vector
-    global non_background_pixels
-
-    rows_unsplit = block["Data"].split("-") # ["1,2,3/1,2,3/1,2,3", "1,2,3/1,2,3/1,2,3"], Each elt: "1,2,3/1,2,3/1,2,3"
-    for row in rows_unsplit:
-        image_rgb_as_strings = row.split("/")   # ["1, 2, 3", "1, 2, 3"] (single row) Each elt: "1, 2, 3"
-        for col in image_rgb_as_strings:
-            pixel_string = col.split(",")   # ["1", "2", "3"] (single row, col location) Each elt: "1"
-            pixel = [int(pixel_string[0]), int(pixel_string[1]), int(pixel_string[2])]
-            electrolyticColorsExtractor(pixel)
-'''
+feature_vector_dictionary = {}  # {1: 7}   {index: count}
 
 def extract_from_block(block):
+    global feature_vector_dictionary
+    
+    feature_vector_dictionary.clear()
     for row in block:
         for pixel in row:
-            electrolyticColorsExtractor(pixel)
+            binColorsUpdate(pixel)
 
-def electrolyticColorsExtractor(pixel):
-    global feature_vector
-    global non_background_pixels
+def binColorsUpdate(pixel):
+    global feature_vector_dictionary
 
-    if not backgroundGreenColor(pixel):  # background subtraction
-        non_background_pixels += 1
-        if electrolyticBlackColor(pixel):
-            feature_vector[0] += 1
-        if ceramicColor(pixel):
-            feature_vector[1] += 1
-        if resistorTan(pixel):
-            feature_vector[2] += 1
-
-def backgroundGreenColor(color):
-    return abs(color[0] - color[1]) < 25 and abs(color[1] - color[2]) < 25 and color[0] > 50 and color[1] > 50 and \
-           color[2] > 50
-
-def electrolyticBlackColor(color):
-    return abs(color[0] - color[1]) < 15 and abs(color[1] - color[2]) < 15 and color[0] < 50 and color[1] < 50 and \
-           color[2] < 50
-
-def ceramicColor(color):
-    return color[0] - color[1] > 25 and color[0] - color[1] < 70 and abs(color[1] - color[2]) < 15 and color[0] < 100
-
-def resistorTan(color):
-    return color[0] - color[1] > 25 and color[0] - color[1] < 60 and abs(color[1] - color[2]) < 15 and color[0] > 100
-
-def normalize_feature_vector():
-    global feature_vector
-    for n in range(len(feature_vector)):
-       feature_vector[n]  = float(feature_vector[n]) / float(non_background_pixels)
+    BIN_SIZE = 16
+    red_index = int(pixel[0] / BIN_SIZE)
+    green_index = int(pixel[1] / BIN_SIZE)
+    blue_index = int(pixel[2] / BIN_SIZE)
+    
+    key = BIN_SIZE*BIN_SIZE*red_index + BIN_SIZE*green_index + blue_index
+    
+    if key in feature_vector_dictionary:
+        feature_vector_dictionary[key] += 1
+    else:
+        feature_vector_dictionary[key] = 1
